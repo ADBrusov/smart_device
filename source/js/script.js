590c5b1a443991079dba502d6f8556ec20317346
Alexander <ADBrusob96@yandex.ru>
2020-12-04 17:57:50 +0300

    Добавляет попап обратной связи

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,64 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// Попап обратной связи
+var popup = document.querySelector('.popup');
+var callbackButton = document.querySelector('.page-header__callback-button');
+var nameInputPopup = popup.querySelector('#name');
 
-pageHeader.classList.remove('page-header--nojs');
+callbackButton.addEventListener('click', function () {
+  var openPopup = function () {
+    popup.classList.add('popup--active');
+  };
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+  var closePopup = function () {
+    popup.classList.remove('popup--active');
+    popup.removeEventListener('click', popupCloseOnClick);
+  };
+
+  var popupCloseOnClick = function (evt) {
+    if (evt.target === evt.currentTarget) {
+      closePopup();
+    }
+  };
+
+  var popupCloseOnKey = function (evt) {
+    if (evt.key === 'Escape' || evt.key === 'x') {
+      closePopup();
+    }
+  };
+
+  openPopup();
+  popup.addEventListener('click', popupCloseOnClick);
+  document.addEventListener('keydown', popupCloseOnKey);
+  nameInputPopup.focus();
+});
+
+// Запись в LocalStorage
+var form = document.querySelector('.callback__form');
+var nameInput = form.querySelector('#name');
+var telephoneInput = form.querySelector('#tel');
+var answerArea = form.querySelector('#answer');
+
+form.addEventListener('submit', function () {
+  localStorage.setItem('name', nameInput.value);
+  localStorage.setItem('telephone', telephoneInput.value);
+  localStorage.setItem('answer', answerArea.value);
+});
+
+// Маска для телефона
+
+
+// Аккордеон для меню в футере
+document.querySelectorAll('.accordeon__button').forEach(function (item) {
+  item.addEventListener('click', function () {
+    var parent = item.parentNode;
+
+    if (parent.classList.contains('accordeon--active')) {
+      parent.classList.remove('accordeon--active');
+    } else {
+      document.querySelectorAll('.accordeon').forEach(function (child) {
+        child.classList.remove('accordeon--active');
+      });
+      parent.classList.add('accordeon--active');
+    }
+  });
 });
