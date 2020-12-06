fa0852304e319a666be8d5e9187d2ce982869b6a
Alexander <ADBrusob96@yandex.ru>
2020-12-06 23:29:44 +0300

    Правки

--- build/js/script.js
+++ build/js/script.js
@@ -1,64 +1,88 @@
 'use strict';
-// Попап обратной связи
-var popup = document.querySelector('.popup');
-var callbackButton = document.querySelector('.page-header__callback-button');
-var nameInputPopup = popup.querySelector('#name');
+(function () {
+  // Попап обратной связи
+  var popup = document.querySelector('.popup');
+  var callbackButton = document.querySelector('.page-header__callback-button');
+  var nameInputPopup = popup.querySelector('#name_popup');
 
-callbackButton.addEventListener('click', function () {
-  var openPopup = function () {
-    popup.classList.add('popup--active');
-  };
+  if (callbackButton && nameInputPopup) {
+    callbackButton.addEventListener('click', function () {
+      var openPopup = function () {
+        popup.classList.add('popup--active');
+        nameInputPopup.focus();
+        popup.addEventListener('click', popupCloseOnClick);
+        document.addEventListener('keydown', popupCloseOnKey);
+      };
 
-  var closePopup = function () {
-    popup.classList.remove('popup--active');
-    popup.removeEventListener('click', popupCloseOnClick);
-  };
+      var closePopup = function () {
+        popup.classList.remove('popup--active');
+        popup.removeEventListener('click', popupCloseOnClick);
+        document.addEventListener('keydown', popupCloseOnKey);
+      };
 
-  var popupCloseOnClick = function (evt) {
-    if (evt.target === evt.currentTarget) {
-      closePopup();
-    }
-  };
+      var popupCloseOnClick = function (evt) {
+        if (evt.target === evt.currentTarget) {
+          closePopup();
+        }
+      };
 
-  var popupCloseOnKey = function (evt) {
-    if (evt.key === 'Escape' || evt.key === 'x') {
-      closePopup();
-    }
-  };
+      var popupCloseOnKey = function (evt) {
+        if (evt.key === 'Escape' || evt.key === 'x') {
+          closePopup();
+        }
+      };
 
-  openPopup();
-  popup.addEventListener('click', popupCloseOnClick);
-  document.addEventListener('keydown', popupCloseOnKey);
-  nameInputPopup.focus();
-});
+      openPopup();
+    });
+  }
 
-// Запись в LocalStorage
-var form = document.querySelector('.callback__form');
-var nameInput = form.querySelector('#name');
-var telephoneInput = form.querySelector('#tel');
-var answerArea = form.querySelector('#answer');
+  // Запись в LocalStorage и маска
+  var forms = document.querySelectorAll('.callback__form');
 
-form.addEventListener('submit', function () {
-  localStorage.setItem('name', nameInput.value);
-  localStorage.setItem('telephone', telephoneInput.value);
-  localStorage.setItem('answer', answerArea.value);
-});
+  if (forms) {
+    forms.forEach(function (form) {
+      var nameInput = form.querySelector('input[name="name"]');
+      var telephoneInput = form.querySelector('input[name="tel"]');
+      var answerArea = form.querySelector('textarea[name="answer"]');
 
-// Маска для телефона
-var telephoneMask = IMask(telephoneInput, {mask: '+{7}(000)000-00-00'});
+      if (telephoneInput && nameInput && answerArea) {
+        form.addEventListener('submit', function () {
+          localStorage.setItem('name', nameInput.value);
+          localStorage.setItem('telephone', telephoneInput.value);
+          localStorage.setItem('answer', answerArea.value);
+        });
 
-// Аккордеон для меню в футере
-document.querySelectorAll('.accordeon__button').forEach(function (item) {
-  item.addEventListener('click', function () {
-    var parent = item.parentNode;
+        telephoneInput.onfocus = function () {
+          telephoneInput.value = '+7(';
+        };
 
-    if (parent.classList.contains('accordeon--active')) {
-      parent.classList.remove('accordeon--active');
-    } else {
-      document.querySelectorAll('.accordeon').forEach(function (child) {
-        child.classList.remove('accordeon--active');
+        telephoneInput.oninput = function () {
+          if (telephoneInput.value.length === 6) {
+            telephoneInput.value = telephoneInput.value + ')';
+          }
+        };
+      }
+    });
+  }
+
+  // Аккордеон для меню в футере
+  var accordeonButtons = document.querySelectorAll('.accordeon__button');
+
+  if (accordeonButtons) {
+    accordeonButtons.forEach(function (accordeonButton) {
+      accordeonButton.addEventListener('click', function () {
+        var parent = accordeonButton.parentNode;
+
+        if (parent.classList.contains('accordeon--active')) {
+          parent.classList.remove('accordeon--active');
+        } else {
+          document.querySelectorAll('.accordeon').forEach(function (child) {
+            child.classList.remove('accordeon--active');
+          });
+          parent.classList.add('accordeon--active');
+        }
       });
-      parent.classList.add('accordeon--active');
-    }
-  });
-});
+    });
+  }
+})();
+
